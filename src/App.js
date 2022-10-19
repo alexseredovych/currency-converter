e96a3ac12f74a43b49ba8a022df328642c1e9fe1
Alex Seredovych <seredovychalex@gmail.com>
2022-10-19 15:24:50 -0500

    Added: - Converter's logic using useState, useEffect, useRef hooks - Simple Sass style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,63 @@
 import React from "react";
+import { Block } from "./Block";
+import './index'
 
 function App() {
+  const [fromCurrency, setFromCurrency] = React.useState("USD");
+  const [toCurrency, setToCurrency] = React.useState("EUR");
+  const [fromPrice, setFromPrice] = React.useState(1);
+  const [toPrice, setToPrice] = React.useState();
+
+  const ratesRef = React.useRef({})
+
+  React.useEffect(() => {
+    fetch("https://cdn.cur.su/api/latest.json")
+      .then((res) => res.json())
+      .then((json) => {
+        ratesRef.current = json.rates
+        onChangeFromPrice(1)
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert("Couldn't retrieve data from the server. Please try again");
+      });
+  }, []);
+
+  const onChangeFromPrice = (value) => {
+    const price = value / ratesRef.current[fromCurrency];
+    const result = price * ratesRef.current[toCurrency];
+    setFromPrice(value);
+    setToPrice(result.toFixed(3));
+  };
+
+  const onChangeToPrice = (value) => {
+    const result = (ratesRef.current[fromCurrency] / ratesRef.current[toCurrency]) * value;
+    setFromPrice(result.toFixed(3));
+    setToPrice(value);
+  };
+
+  React.useEffect(() => {
+    onChangeFromPrice(fromPrice);
+  }, [fromCurrency]);
+
+  React.useEffect(() => {
+    onChangeToPrice(toPrice)
+  }, [toCurrency])
+
   return (
     <div className="App">
-      
+      <Block
+        value={fromPrice}
+        currency={fromCurrency}
+        onChangeCurrency={setFromCurrency}
+        onChangeValue={onChangeFromPrice}
+      />
+      <Block
+        value={toPrice}
+        currency={toCurrency}
+        onChangeCurrency={setToCurrency}
+        onChangeValue={onChangeToPrice}
+      />
     </div>
   );
 }
